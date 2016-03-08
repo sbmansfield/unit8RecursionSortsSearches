c708f1a0d6e926926bf12cda6b668358f3d00a79
sbmansfield <SBMansfield@student.naperville203.org>
2016-03-08 15:09:26 -0600

    3/8

--- PalindromeTester/PalindromeTester.java
+++ PalindromeTester/PalindromeTester.java
@@ -14,7 +14,6 @@ public class PalindromeTester
    public static void main (String[] args)
    {
       String str, another = "y";
-      int left, right;
       Scanner s = new Scanner(System.in);
 
       while (another.equalsIgnoreCase("y")) // allows y or Y
@@ -22,25 +21,30 @@ public class PalindromeTester
          System.out.println ("Enter a potential palindrome:");
          str = s.nextLine();
 
-         left = 0;
-         right = str.length() - 1;
-
-         while (str.charAt(left) == str.charAt(right) && left < right)
+         if (isPalindrome(str) == true)
          {
-            left++;
-            right--;
+             System.out.println ("That string IS a palindrome.");
          }
-
-         System.out.println();
-
-         if (left < right)
-            System.out.println ("That string is NOT a palindrome.");
          else
-            System.out.println ("That string IS a palindrome.");
+         {
+             System.out.println ("That string is NOT a palindrome.");
+         }
 
          System.out.println();
          System.out.print ("Test another palindrome (y/n)? ");
          another = s.nextLine();
       }
+   }
+   
+   public static boolean isPalindrome(String str)
+   {
+       if (str.length() <= 1)
+       {
+           return true;
+       }
+       
+       String substr = str.substring(1, str.length() - 1);
+       
+       return ( (str.charAt(0) == str.charAt(str.length() - 1)) && isPalindrome(substr));
    }
 }
